b06e50b918d17da098c436b438b7f8efc2ee6d75
ThierryO <Thierry.Onkelinx@inbo.be>
2016-09-15 14:16:54 +0200

    create minimal example

---
+++ R/bookdown_test.R
@@ -0,0 +1,31 @@
+#' A test style for bookdown
+#' @importFrom rmarkdown output_format knitr_options pandoc_options
+#' @export
+bookdown_test <- function(...){
+  template <- system.file("pandoc/bookdown_test.tex", package = "bookdowntest")
+  args <- c(
+    "--template", template,
+    "--latex-engine", "xelatex"
+  )
+  opts_chunk <- list(
+    dev = 'pdf',
+    dpi = 300,
+    fig.width = 4.5,
+    fig.height = 2.9
+  )
+  output_format(
+    knitr = knitr_options(
+      opts_knit = list(
+        width = 60,
+        concordance = TRUE
+      ),
+      opts_chunk = opts_chunk
+    ),
+    pandoc = pandoc_options(
+      to = "latex",
+      args = args,
+      keep_tex = TRUE
+    ),
+    clean_supporting = FALSE
+  )
+}
